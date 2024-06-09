bf10230fb0515017d53f63cc87d9b548d502e29e
Gabriel Aranha <gabriel.aranha.martins@gmail.com>
2024-06-09 14:36:10 -0300

    improve logs

--- src/api.rs
+++ src/api.rs
@@ -3,21 +3,26 @@ use crate::integrations::aws::AwsIpRanges;
 use rocket::serde::json::serde_json;
 use crate::cache::IntegrationCache;
 use crate::cache::CACHE;
-use tracing::{error, info};
 use uuid::Uuid;
 
 #[get("/aws?<region>&<service>&<network_border_group>")]
 fn query_aws_data(
-    region: Option<String>, 
-    service: Option<String>, 
+    region: Option<String>,
+    service: Option<String>,
     network_border_group: Option<String>,
 ) -> Option<String> {
     // Generate a unique request ID
     let request_id = Uuid::new_v4();
-    
-    // Log the request ID
-    info!("Request ID: {}", request_id);
-    
+
+    // Log the start of the request with structured fields for received parameters
+    tracing::info!(
+        request_id = %request_id,
+        region = region.clone().unwrap_or_else(|| "None".to_string()),
+        service = service.clone().unwrap_or_else(|| "None".to_string()),
+        network_border_group = network_border_group.clone().unwrap_or_else(|| "None".to_string()),
+        "Received request"
+    );
+
     // Read the global cache
     let cache = CACHE.clone();
 
@@ -27,31 +32,48 @@ fn query_aws_data(
         if let Some(aws_cache) = aws_data_ref.downcast_ref::<IntegrationCache<AwsIpRanges>>() {
             // Filter the AWS data based on the provided parameters
             let filtered_data: Vec<&str> = aws_cache.data.as_ref().map_or_else(Vec::new, |data| {
-                data.prefixes.iter().filter_map(|prefix| {
-                    // Check if the data matches the provided parameters
-                    let matches = region.as_deref().map_or(true, |param| &prefix.region == param)
-                        && service.as_deref().map_or(true, |param| &prefix.service == param)
-                        && network_border_group.as_deref().map_or(true, |param| &prefix.network_border_group == param);
+                data.prefixes
+                    .iter()
+                    .filter_map(|prefix| {
+                        // Check if the data matches the provided parameters
+                        let matches = region
+                            .as_deref()
+                            .map_or(true, |param| prefix.region == param)
+                            && service
+                                .as_deref()
+                                .map_or(true, |param| prefix.service == param)
+                            && network_border_group
+                                .as_deref()
+                                .map_or(true, |param| prefix.network_border_group == param);
 
-                    if matches {
-                        // Return the IP prefix as &str
-                        Some(prefix.ip_prefix.as_str())
-                    } else {
-                        None
-                    }
-                }).collect()
+                        if matches {
+                            // Return the IP prefix as &str
+                            Some(prefix.ip_prefix.as_str())
+                        } else {
+                            None
+                        }
+                    })
+                    .collect()
             });
 
             // Serialize the filtered data to JSON string
             if !filtered_data.is_empty() {
-                info!("AWS data found for request ID: {}", request_id);
+                tracing::info!(
+                    request_id = %request_id,
+                    "AWS data found for request"
+                );
                 return serde_json::to_string(&filtered_data).ok();
             }
         }
     }
-    error!("Failed to retrieve AWS data for request ID: {}", request_id);
+    // Log failure to retrieve AWS data
+    tracing::error!(
+        request_id = %request_id,
+        "Failed to retrieve AWS data"
+    );
     None
 }
+
 
 pub fn routes() -> Vec<Route> {
     routes![
