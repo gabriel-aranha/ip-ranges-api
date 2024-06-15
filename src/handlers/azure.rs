8a98ab0e773e45d5d6c38313e03c056eda039a0f
Gabriel Aranha <gabriel.aranha.martins@gmail.com>
2024-06-15 00:24:52 -0300

    working

--- src/handlers/azure.rs
+++ src/handlers/azure.rs
@@ -32,10 +32,11 @@ pub fn query_azure_data(
     // Access the Azure cache from the global cache
     if let Some(azure_data_ref) = cache.get("azure") {
         // Extract the Azure data
-        if let Some(azure_cache) = azure_data_ref.downcast_ref::<IntegrationCache<AzureIpRanges>>() {
+        if let Some(azure_cache) = azure_data_ref.downcast_ref::<IntegrationCache<AzureIpRanges>>()
+        {
             // Filter the Azure data based on the provided parameters
             let filtered_data: Vec<String> = azure_cache.data.as_ref().map_or_else(Vec::new, |data| {
-                data.values.iter().filter_map(|value| {
+                let mut filtered_data: Vec<String> = data.values.iter().filter_map(|value| {
                     let param_region = region.clone().map(|s| s.to_lowercase());
                     let param_system_service = system_service.clone().map(|s| s.to_lowercase());
 
@@ -55,42 +56,44 @@ pub fn query_azure_data(
                     } else {
                         None
                     }
-                }).collect()
+                }).collect();
+
+                let ipv4_flag = ipv4.unwrap_or(false);
+                let ipv6_flag = ipv6.unwrap_or(false);
+
+                // Further filter based on IPv4 and IPv6 flags
+                if !ipv4_flag && !ipv6_flag {
+                    info!(request_id = %request_id, "Returning empty response as both IPv4 and IPv6 filters are false");
+                    return Vec::new(); // Return an empty vector
+                }
+
+                filtered_data.iter_mut().for_each(|prefixes| {
+                    let prefix_list: Vec<&str> = prefixes.split(',').collect();
+                    let filtered_prefix_list: Vec<&str> = prefix_list.into_iter().filter(|prefix| {
+                        let is_ipv4 = is_ipv4(prefix);
+                        if ipv4_flag && !ipv6_flag {
+                            is_ipv4
+                        } else if !ipv4_flag && ipv6_flag {
+                            !is_ipv4
+                        } else {
+                            true
+                        }
+                    }).collect();
+                    *prefixes = filtered_prefix_list.join(",");
+                });
+
+                info!(request_id = %request_id, "Filtered data: {:?}", filtered_data);
+
+                filtered_data
             });
 
-            // Determine IPv4 and IPv6 filtering flags
-            let ipv4_flag = ipv4.unwrap_or(false);
-            let ipv6_flag = ipv6.unwrap_or(false);
-
-            // Apply filtering based on IPv4 and IPv6 flags
-            let mut filtered_data_iterated: Vec<String> = Vec::new();
-
-            for prefixes in filtered_data {
-                if ipv4_flag && !ipv6_flag {
-                    let filtered_prefixes: Vec<&str> = prefixes.split(',').filter(|&prefix| is_ipv4(prefix.trim())).collect();
-                    if !filtered_prefixes.is_empty() {
-                        filtered_data_iterated.push(filtered_prefixes.join(","));
-                    }
-                } else if !ipv4_flag && ipv6_flag {
-                    let filtered_prefixes: Vec<&str> = prefixes.split(',').filter(|&prefix| !is_ipv4(prefix.trim())).collect();
-                    if !filtered_prefixes.is_empty() {
-                        filtered_data_iterated.push(filtered_prefixes.join(","));
-                    }
-                }
-            }
-
             // Serialize the filtered data to JSON string
-            if !filtered_data_iterated.is_empty() {
+            if !filtered_data.is_empty() {
                 info!(
                     request_id = %request_id,
                     "Azure data found for request"
                 );
-                return serde_json::to_string(&filtered_data_iterated).ok();
-            } else {
-                info!(
-                    request_id = %request_id,
-                    "Filtered data is empty after applying filters"
-                );
+                return serde_json::to_string(&filtered_data).ok();
             }
         }
     }
