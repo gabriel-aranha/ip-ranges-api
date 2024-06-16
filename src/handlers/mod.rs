6d8b91b98970e8acf8f7be589950a26e74a0b85d
Gabriel Aranha <gabriel.aranha.martins@gmail.com>
2024-06-16 10:27:21 -0300

    Update mod.rs

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -5,7 +5,7 @@ pub mod gcp;
 
 use crate::handlers::aws::query_aws_data;
 use crate::handlers::azure::query_azure_data;
-use crate::habdlers::health::health_check;
+use crate::handlers::health::health_check;
 use crate::handlers::gcp::query_gcp_data;
 
 use rocket::{routes, Route};
