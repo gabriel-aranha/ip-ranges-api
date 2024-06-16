4d29cb19156eb96ec1e6cc304b6131220333dfc5
Gabriel Aranha <gabriel.aranha.martins@gmail.com>
2024-06-16 10:24:33 -0300

    Update mod.rs

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,13 +1,15 @@
 pub mod aws;
 pub mod azure;
+pub mod health;
 pub mod gcp;
 
 use crate::handlers::aws::query_aws_data;
 use crate::handlers::azure::query_azure_data;
+use crate::habdlers::health::health_check;
 use crate::handlers::gcp::query_gcp_data;
 
 use rocket::{routes, Route};
 
 pub fn routes() -> Vec<Route> {
-    routes![query_aws_data, query_azure_data, query_gcp_data]
+    routes![query_aws_data, query_azure_data, health_check, query_gcp_data]
 }
